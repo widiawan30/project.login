feda5b9cf439376454efa962e77e391d6c8b8e64
widiawan30 <fadliwdwn@gmail.com>
2023-10-30 01:17:48 +0700

    project login fadli

---
+++ src/login.js
@@ -0,0 +1,16 @@
+function getData() {
+  const username = document.getElementById("input-username");
+  const password = document.getElementById("input-password");
+  const submit = document.getElementById("input-submit");
+
+  let data = {
+    username,
+    password,
+    submit,
+  };
+
+  const emailReceiver = "hi.muhammadfadliwidiawan@gmail.com"
+  lat a = document.createElement("a")
+
+  console.log(data);
+}
